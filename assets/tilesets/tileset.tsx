ff150ca7c0d0309f6d7a236d3c86d66e9006f5f4
Zstudios-gg <Zstudios-gg@users.noreply.github.com>
2026-04-26 00:22:16 +0000

    deploy: 441448a9233b29d687f83e731f510a3955885cec

---
+++ assets/tilesets/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="tileset" tilewidth="16" tileheight="16" tilecount="420" columns="20">
+ <image source="../../../../ZStudios/XatruchRPG ARTE/Ciudad Principal/tileset.png" width="320" height="336"/>
+</tileset>
